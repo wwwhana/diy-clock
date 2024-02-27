b6fc58e3755c0c503601fd844ccca18ac0039959
HaNAst.L <limlexia@gmail.com>
2024-02-28 02:08:44 +0900

    Update 7digit.ino
    add refresh rate. add command mode("[BR]","[BLINK]", "[DEBUG]"). 
    Serial => Serial1. Serial is Debug Only.
    migration serialEvent1

--- 7digit.ino
+++ 7digit.ino
@@ -4,47 +4,102 @@ SevSeg sevseg;
 String display = "NREADY__";
 int decPlaces = 0;
 uint8_t segs[8] = {};
+bool debug = false;
+
+unsigned long previousMillis = 0;  // will store last time LED was updated
+const long interval = 20;          // interval at which to blink (milliseconds)
+
 
 void setup() {
-  Serial.begin(19200);
-  Serial.setTimeout(5000);
+  Serial.begin(9600);
+  Serial1.begin(9600);
 
   byte numDigits = 8;
   byte digitPins[] = { 5, 4, 3, 2, 9, 8, 7, 6 };
   byte segmentPins[] = { 20, 21, 16, 10, 14, 18, 15, 19 };
 
   bool resistorsOnSegments = true;
-  byte hardwareConfig = COMMON_ANODE;
+  byte hardwareConfig = NP_COMMON_CATHODE;
 
-  bool updateWithDelays = false;  // Default 'false' is Recommended
-  bool leadingZeros = false;      // Use 'true' if you'd like to keep the leading zeros
-  bool disableDecPoint = false;   // Use 'true' if your decimal point doesn't exist or isn't connected. Then, you only need to specify 7 segmentPins[]
+  bool updateWithDelays = true;  // Default 'false' is Recommended
+  bool leadingZeros = false;     // Use 'true' if you'd like to keep the leading zeros
+  bool disableDecPoint = false;  // Use 'true' if your decimal point doesn't exist or isn't connected. Then, you only need to specify 7 segmentPins[]
 
 
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
                updateWithDelays, leadingZeros, disableDecPoint);
-}
 
-void loop() {
-
-  if (Serial.available() > 0) {
-    display = Serial.readStringUntil('|');
-    decPlaces = Serial.readStringUntil('\n').toInt();
-  } 
-
-
+  sevseg.setBrightness(40);
 
   sevseg.setChars(display.c_str());
-  sevseg.getSegments(segs);
-
-  for (byte i = 0; i < 8 && decPlaces > 0; i++) {
-    byte state = bitRead(decPlaces, i);
-    if (state) {
-      segs[7 - i] = segs[7 - i] |= 0b10000000;
-    }
-  }
-
-  sevseg.setSegments(segs);
   sevseg.refreshDisplay();
 }
 
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (debug && Serial.availableForWrite()) {
+      Serial.println(previousMillis);
+    }
+
+    sevseg.refreshDisplay();
+  }
+}
+
+void serialEvent1() {
+  if (Serial1.available() > 0) {
+    String display_recv = Serial1.readStringUntil('|');
+    String decPlaces_recv_str = Serial1.readStringUntil('\n');
+    int decPlaces_recv = decPlaces_recv_str.toInt();
+
+
+    if (display_recv == "[BR]") {
+
+      sevseg.setBrightness(decPlaces_recv);
+
+      return;
+    } else if (display_recv == "[BLINK]") {
+      sevseg.blank();
+      display = "";
+      decPlaces = 0;
+
+      return;
+    // } else if (display_recv == "[INTER]") {
+    //   interval = decPlaces_recv;
+
+    //   return;
+    } else if (display_recv == "[DEBUG]") {
+      debug = !debug;
+
+      return;
+    }
+
+    if (debug) {
+      if (Serial.availableForWrite()) {
+
+        Serial.println(display_recv.c_str());
+        Serial.println(decPlaces_recv_str.c_str());
+      } else {
+        debug = false;
+      }
+    }
+
+
+    display = display_recv;
+    sevseg.setChars(display.c_str());
+
+    decPlaces = decPlaces_recv;
+    sevseg.getSegments(segs);
+    for (byte i = 0; i < 8 && decPlaces > 0; i++) {
+      byte state = bitRead(decPlaces, i);
+      if (state) {
+        segs[7 - i] = segs[7 - i] |= 0b10000000;
+      }
+    }
+
+    sevseg.setSegments(segs);
+  }
+}
