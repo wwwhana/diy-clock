bd0fe85cd9ff5d9d88167e7212b6bfb85c82ca70
HaNAst.L <limlexia@gmail.com>
2024-03-06 22:32:33 +0900

    update 7segment firmware. new command

--- 7digit.ino
+++ 7digit.ino
@@ -1,13 +1,15 @@
+#include <TimeLib.h>
 #include "SevSeg.h"
 SevSeg sevseg;
 
 String display = "NREADY__";
+String mode = "CLOCK";
 int decPlaces = 0;
 uint8_t segs[8] = {};
-bool debug = false;
 
 unsigned long previousMillis = 0;  // will store last time LED was updated
 const long interval = 20;          // interval at which to blink (milliseconds)
+int bright = 40;
 
 
 void setup() {
@@ -18,8 +20,10 @@ void setup() {
   byte digitPins[] = { 15, 14, 16, 10, 21, 20, 19, 18 };
   byte segmentPins[] = { 3, 2, 8, 9, 7, 5, 6, 4 };
 
+
+
   bool resistorsOnSegments = true;
-  byte hardwareConfig = NP_COMMON_CATHODE;
+  byte hardwareConfig = COMMON_ANODE;
 
   bool updateWithDelays = true;  // Default 'false' is Recommended
   bool leadingZeros = false;     // Use 'true' if you'd like to keep the leading zeros
@@ -29,77 +33,145 @@ void setup() {
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
                updateWithDelays, leadingZeros, disableDecPoint);
 
-  sevseg.setBrightness(40);
+  sevseg.setBrightness(bright);
 
   sevseg.setChars(display.c_str());
   sevseg.refreshDisplay();
 }
 
 void loop() {
+
+
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
 
-    if (debug && Serial.availableForWrite()) {
-      Serial.println(previousMillis);
+    if (mode == "BLINK") {
+      display = "";
+      decPlaces = 0;
+      sevseg.blank();
+    } else if (mode == "STR") {
+
+      Serial.println(display);
+
+      sevseg.setChars(display.c_str());
+      sevseg.getSegments(segs);
+
+      setDecPoint(segs, decPlaces);
+
+
+      sevseg.setSegments(segs);
+    } else {
+      display = "NREADY__";
+      decPlaces = 0;
+
+      char buffer[8];
+      time_t t = now();
+
+      int seconds = second(t);
+
+      sprintf(buffer, "%02d%02d%02d%02d", hour(t), minute(t), month(t), day(t));
+      sevseg.setChars(buffer);
+      sevseg.getSegments(segs);
+
+      int flag = 2 | ((seconds % 2 == 0) ? 32 : 0 );
+
+      setDecPoint(segs, flag);
+
+      sevseg.setSegments(segs);
     }
 
     sevseg.refreshDisplay();
   }
 }
 
+void setDecPoint(uint8_t segs[8], int flag) {
+  if (flag < 0 || flag > 255) return;
+
+  for (byte i = 0; i < 8 && flag > 0; i++) {
+    byte state = bitRead(flag, i);
+    if (state) {
+      segs[7 - i] = segs[7 - i] | 0b10000000;
+    }
+  }
+}
+
 void serialEvent1() {
   if (Serial1.available() > 0) {
-    String display_recv = Serial1.readStringUntil('|');
-    String decPlaces_recv_str = Serial1.readStringUntil('\n');
-    int decPlaces_recv = decPlaces_recv_str.toInt();
+    String mode = Serial1.readStringUntil('|');
+    Serial.println(mode);
+    // Serial1.print(mode);
 
-
-    if (display_recv == "[BR]") {
-
-      sevseg.setBrightness(decPlaces_recv);
-
-      return;
-    } else if (display_recv == "[BLINK]") {
-      sevseg.blank();
-      display = "";
-      decPlaces = 0;
-
-      return;
-    // } else if (display_recv == "[INTER]") {
-    //   interval = decPlaces_recv;
-
-    //   return;
-    } else if (display_recv == "[DEBUG]") {
-      debug = !debug;
-
-      return;
+    if (mode == "SETTIME") {
+      setTime();
+    } else if (mode == "SETBR") {
+      setBright();
+    } else if (mode == "SETMODE") {
+      setMode();
+    } else if (mode == "SETSTR") {
+      setSTR();
+    } else if (mode == "GETTIME") {
+      getTime();
+    } else {
     }
-
-    if (debug) {
-      if (Serial.availableForWrite()) {
-
-        Serial.println(display_recv.c_str());
-        Serial.println(decPlaces_recv_str.c_str());
-      } else {
-        debug = false;
-      }
-    }
-
-
-    display = display_recv;
-    sevseg.setChars(display.c_str());
-
-    decPlaces = decPlaces_recv;
-    sevseg.getSegments(segs);
-    for (byte i = 0; i < 8 && decPlaces > 0; i++) {
-      byte state = bitRead(decPlaces, i);
-      if (state) {
-        segs[7 - i] = segs[7 - i] |= 0b10000000;
-      }
-    }
-
-    sevseg.setSegments(segs);
   }
+}
+
+void getTime() {
+  char buffer[20];
+  time_t t = now();
+
+  sprintf(buffer, "%04d.%02d.%02d %02d:%02d:%02d", year(t), month(t), day(t), hour(t), minute(t), second(t));
+
+  if (Serial1.availableForWrite() > 0) {
+    Serial1.println(buffer);
+  }
+
+  Serial.println(buffer);
+}
+
+
+void setTime() {
+  int year = Serial1.readStringUntil('.').toInt();
+  int month = Serial1.readStringUntil('.').toInt();
+  int day = Serial1.readStringUntil(' ').toInt();
+  int hour = Serial1.readStringUntil(':').toInt();
+  int min = Serial1.readStringUntil(':').toInt();
+  int sec = Serial1.readStringUntil('\n').toInt();
+
+  char buffer[30];
+  // sprintf(buffer, "%4d%2d%2d %2d:%2d:%2d", year, month, day, hour, min, sec);
+  // Serial.println(buffer);
+
+  setTime(hour, min, sec, day, month, year);
+}
+
+void setMode() {
+  String tmp = Serial1.readStringUntil('\n');
+  tmp.toUpperCase();
+
+  mode = tmp;
+}
+
+void setBright() {
+  int tmp_bright = Serial1.readStringUntil('\n').toInt();
+
+  if (tmp_bright < -200) {
+    tmp_bright = -200;
+  } else if (tmp_bright > 200) {
+    tmp_bright = 200;
+  }
+
+  bright = tmp_bright;
+
+  sevseg.setBrightness(bright);
+}
+
+
+void setSTR() {
+  String tmp_str = Serial1.readStringUntil('|');
+  int tmp_decPlaces = Serial1.readStringUntil('\n').toInt();
+  display = tmp_str;
+  decPlaces = tmp_decPlaces;
 }
