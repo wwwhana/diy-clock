e8df5f6a0ffb7753f72e21a97885905e1577a606
HaNAst.L <limlexia@gmail.com>
2024-03-14 21:56:04 +0900

    add cron about temperature & humidity set up

--- clock.py
+++ clock.py
@@ -4,6 +4,7 @@ import serial
 import datetime
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
+import dht11
 
 #시리얼포트 객체 ser을 생성
 #pc와 스위치 시리얼포트 접속정보
@@ -16,19 +17,20 @@ ser = serial.Serial(
     timeout=8
     )
 
-pin = 10
+lightSensorPin = 10
+Dht11Pin = 17
 INPUT = 0
 OUTPUT = 1
 
 def initClock():
   wiringpi.wiringPiSetup()
-  wiringpi.pinMode(pin, INPUT)
+  wiringpi.pinMode(lightSensorPin, INPUT)
 
-  cmd = f"SETMODE|CLOCK\n"
+  cmd = f"SETMODE|DETAILCLOCK\n"
   ser.write(str.encode(cmd))
 
 def readBright():
-  return wiringpi.digitalRead(pin)
+  return wiringpi.digitalRead(lightSensorPin)
 
 def syncTime():
    now = datetime.datetime.now()
@@ -49,20 +51,35 @@ def setBright():
 
   ser.write(str.encode(f"SETBR|{bright}\n"))
 
+def setTempertureHumidity():
+  done = False
+
+  while not done:
+    result = sensor.read()
+    if result.is_valid():
+      print(f"{result.humidity}|{result.temperature}")
+      ser.write(str.encode(f"SETHUMIDITY|{result.humidity}\n"))
+      time.sleep(1)
+      ser.write(str.encode(f"SETTEMPERATURE|{result.temperature}\n"))
+      print("X")
+      done = True
 
 ser.isOpen()
+sensor = dht11.DHT11(pin = Dht11Pin)
+
 startup = datetime.datetime.now()
 
 initClock()
 setBright()
 syncTime()
-
+setTempertureHumidity()
 
 sched = BackgroundScheduler()
 sched.start()
 
 sched.add_job(syncTime, 'interval', seconds=10, id="tick_event")
 sched.add_job(setBright, 'interval', seconds=5, id="bright_event")
+sched.add_job(setTempertureHumidity, 'interval', seconds=5, id="temperture_humidity_event")
 
 before  = datetime.datetime.now()
 
@@ -72,6 +89,7 @@ while True:
   before = now
 
   if diff > 3:
+    syncTime()
     exit(1)
 
   time.sleep(1)
