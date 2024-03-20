18fd8ee8cf86b8c2f788ce08bbfa3bf662ab2ea1
HaNAst.L <limlexia@gmail.com>
2024-03-20 23:13:31 +0900

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,15 +1,13 @@
-import wiringpi
-import subprocess
 import serial
 import datetime
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
-import dht11
+import json
 
 #시리얼포트 객체 ser을 생성
 #pc와 스위치 시리얼포트 접속정보
 ser = serial.Serial(
-    port = '/dev/ttyS3',
+    port = '/dev/ttyS1',
     baudrate=9600,
     parity='N',
     stopbits=1,
@@ -17,20 +15,30 @@ ser = serial.Serial(
     timeout=8
     )
 
-lightSensorPin = 10
-Dht11Pin = 17
-INPUT = 0
-OUTPUT = 1
-
 def initClock():
-  wiringpi.wiringPiSetup()
-  wiringpi.pinMode(lightSensorPin, INPUT)
 
   cmd = f"SETMODE|DETAILCLOCK\n"
   ser.write(str.encode(cmd))
 
-def readBright():
-  return wiringpi.digitalRead(lightSensorPin)
+def readSensor():
+  while True:
+    try:
+      line = ser.readline().strip()
+      print(line)
+      if line == b'':
+        continue
+      r = json.loads(line)
+      with open("/opt/clock/sensors.json", "w") as f:
+        f.write(line.decode('utf-8'))
+        break
+    except Exception as e:
+      print(e)
+      pass
+
+def isShadow():
+  with open("/opt/clock/sensors.json", "r") as f:
+    line = f.readline()
+    return json.loads(line)["shadow"]
 
 def syncTime():
    now = datetime.datetime.now()
@@ -41,45 +49,32 @@ def setBright():
   bright = 0
   now = datetime.datetime.now()
 
-  if readBright() == 0:
-    bright = 70
-  else:
+  if isShadow() is False:
     if now.hour >= 22 or now.hour < 7:
       bright = 0
     else:
-      bright = 30
+      bright = 70
+  else:
+    bright = 30
 
   ser.write(str.encode(f"SETBR|{bright}\n"))
 
-def setTempertureHumidity():
-  done = False
 
-  while not done:
-    result = sensor.read()
-    if result.is_valid():
-      print(f"{result.humidity}|{result.temperature}")
-      ser.write(str.encode(f"SETHUMIDITY|{result.humidity}\n"))
-      time.sleep(1)
-      ser.write(str.encode(f"SETTEMPERATURE|{result.temperature}\n"))
-      print("X")
-      done = True
 
 ser.isOpen()
-sensor = dht11.DHT11(pin = Dht11Pin)
 
 startup = datetime.datetime.now()
 
 initClock()
 setBright()
 syncTime()
-setTempertureHumidity()
 
 sched = BackgroundScheduler()
 sched.start()
 
-sched.add_job(syncTime, 'interval', seconds=10, id="tick_event")
-sched.add_job(setBright, 'interval', seconds=5, id="bright_event")
-sched.add_job(setTempertureHumidity, 'interval', seconds=5, id="temperture_humidity_event")
+sched.add_job(syncTime, 'interval', seconds=300, id="sync_event")
+sched.add_job(readSensor, 'interval', seconds=3, id="tick_event")
+sched.add_job(setBright, 'interval', seconds=4, id="bright_event")
 
 before  = datetime.datetime.now()
 
